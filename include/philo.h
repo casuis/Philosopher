beb5e5e836e693e1f50ec76379a56871a14f15ba
Arthur Simon <rthrsmn@gmail.com>
2022-01-27 20:05:58 +0100

    add includes file

---
+++ include/philo.h
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asimon <asimon@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/27 17:41:24 by asimon            #+#    #+#             */
+/*   Updated: 2022/01/27 17:41:43 by asimon           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <pthread.h>
+# include <sys/time.h>
+# include <string.h>
+
+typedef struct s_arg
+{
+    int     nb_philo;
+    int     t_dead;
+    int     t_eat;
+    int     t_sleep;
+    int     need_eat;
+}               t_arg;
+
+typedef struct s_shared
+{
+    t_arg               arg;
+    int                 *is_dead;
+    int                 *eat_enough;
+    pthread_mutex_t     *dead;
+    pthread_mutex_t     *write_protect;
+}               t_shared;
+
+typedef struct s_philo
+{
+    int                 balise;
+    int                 index;
+    int                 count;
+    int                 last_meal;
+    t_shared            *shared;
+    pthread_t           thread;
+    struct timeval      tmstp;
+    pthread_mutex_t     fork;
+    struct s_philo      *next;
+
+}               t_philo;
+
+t_philo     *set_list(int size, t_shared *shared);
+t_shared    *set_shared(t_arg arg);
+void        *routine(void *philo);
+int         get_timestamp(struct timeval start);
+void        unlock(t_philo *philo);
+void        lock(t_philo *philo);
+void        protect_write(t_philo *philo, char *str);
+int         ft_atoi(char *str);
+void        monitoring(t_philo *philo);
+void        exit_thread(t_philo *philo);
+
+#endif
