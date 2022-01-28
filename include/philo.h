3d9c40fd3e72a26ae975c9ebd8a0908dac13d10b
Arthur Simon <rthrsmn@gmail.com>
2022-01-28 18:57:21 +0100

    add philo.h normed

--- include/philo.h
+++ include/philo.h
@@ -22,44 +22,43 @@
 
 typedef struct s_arg
 {
-    int     nb_philo;
-    int     t_dead;
-    int     t_eat;
-    int     t_sleep;
-    int     need_eat;
-}               t_arg;
+	int		nb_philo;
+	int		t_dead;
+	int		t_eat;
+	int		t_sleep;
+	int		need_eat;
+}	t_arg;
 
 typedef struct s_shared
 {
-    t_arg               arg;
-    int                 *is_dead;
-    int                 *eat_enough;
-    pthread_mutex_t     *dead;
-    pthread_mutex_t     *write_protect;
-}               t_shared;
+	t_arg				arg;
+	int					*is_dead;
+	int					*eat_enough;
+	pthread_mutex_t		*dead;
+	pthread_mutex_t		*write_protect;
+}	t_shared;
 
 typedef struct s_philo
 {
-    int                 balise;
-    int                 index;
-    int                 count;
-    int                 last_meal;
-    t_shared            *shared;
-    pthread_t           thread;
-    struct timeval      tmstp;
-    pthread_mutex_t     fork;
-    struct s_philo      *next;
+	int					balise;
+	int					index;
+	int					count;
+	int					last_meal;
+	t_shared			*shared;
+	pthread_t			thread;
+	struct timeval		tmstp;
+	pthread_mutex_t		fork;
+	struct s_philo		*next;
+}	t_philo;
 
-}               t_philo;
-
-void        philosopher(t_arg arg);
-t_philo     *set_list(int size, t_shared *shared);
-t_shared    *set_shared(t_arg arg);
-void        *routine(void *philo);
-int         get_timestamp(struct timeval start);
-void        unlock(t_philo *philo);
-void        lock(t_philo *philo);
-void        protect_write(t_philo *philo, char *str);
-int         ft_atoi(char *str);
+void		philosopher(t_arg arg);
+t_philo		*set_list(int size, t_shared *shared);
+t_shared	*set_shared(t_arg arg);
+void		*routine(void *philo);
+int			get_timestamp(struct timeval start);
+void		unlock(t_philo *philo);
+void		lock(t_philo *philo);
+void		protect_write(t_philo *philo, char *str);
+int			ft_atoi(char *str);
 
 #endif
