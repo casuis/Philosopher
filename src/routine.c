2da7e4d81a7727fe505a080e5ee68b3548d9cf2e
Arthur Simon <rthrsmn@gmail.com>
2022-01-28 18:52:47 +0100

    add routine normed

--- src/routine.c
+++ src/routine.c
@@ -12,46 +12,46 @@
 
 #include "../include/philo.h"
 
-void    eating(t_philo *philo)
+void	eating(t_philo *philo)
 {
-    lock(philo);
-    if (*(philo->shared->is_dead) != 1 && *(philo->shared->eat_enough) != 1)
-    {
-        pthread_mutex_lock(philo->shared->dead);
-        philo->last_meal = get_timestamp(philo->tmstp);
-        philo->count += 1;
-        protect_write(philo, "%d Philo %d is eating\n");
-        pthread_mutex_unlock(philo->shared->dead);
-        usleep(philo->shared->arg.t_eat * 999);
-    }
-    unlock(philo);
+	lock(philo);
+	if (*(philo->shared->is_dead) != 1 && *(philo->shared->eat_enough) != 1)
+	{
+		pthread_mutex_lock(philo->shared->dead);
+		philo->last_meal = get_timestamp(philo->tmstp);
+		philo->count += 1;
+		protect_write(philo, "%d Philo %d is eating\n");
+		pthread_mutex_unlock(philo->shared->dead);
+		usleep(philo->shared->arg.t_eat * 999);
+	}
+	unlock(philo);
 }
 
-void    sleeping(t_philo *philo)
+void	sleeping(t_philo *philo)
 {
-    protect_write(philo,"%d Philo %d is sleeping\n");
-    usleep(philo->shared->arg.t_sleep * 999);
+	protect_write(philo, "%d Philo %d is sleeping\n");
+	usleep(philo->shared->arg.t_sleep * 999);
 }
 
-void    thinking(t_philo *philo)
-{   
-    protect_write(philo, "%d Philo %d is thinking\n");
+void	thinking(t_philo *philo)
+{
+	protect_write(philo, "%d Philo %d is thinking\n");
 }
 
-void    *routine(void *philo)
+void	*routine(void *philo)
 {
-    t_philo     *buff;
+	t_philo		*buff;
 
-    buff = (t_philo *)philo;
-    if (buff->index % 2 == 0)
-        usleep(1000);
-    while (*(buff->shared->is_dead) != 1 && *(buff->shared->eat_enough) != 1)
-    {
-        eating(buff);
-        if (*(buff->shared->is_dead) != 1 && *(buff->shared->eat_enough) != 1)
-            sleeping(buff);
-        if (*(buff->shared->is_dead) != 1 && *(buff->shared->eat_enough) != 1)
-            thinking(buff);
-    }
-    return (philo);
+	buff = (t_philo *)philo;
+	if (buff->index % 2 == 0)
+		usleep(1000);
+	while (*(buff->shared->is_dead) != 1 && *(buff->shared->eat_enough) != 1)
+	{
+		eating(buff);
+		if (*(buff->shared->is_dead) != 1 && *(buff->shared->eat_enough) != 1)
+			sleeping(buff);
+		if (*(buff->shared->is_dead) != 1 && *(buff->shared->eat_enough) != 1)
+			thinking(buff);
+	}
+	return (philo);
 }
